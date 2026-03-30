36636e7ebe61a40c3eff5fdfb006e03104e1013e
mabeyncafe <mabeyncafe58@gmail.com>
2026-03-30 23:33:54 +0300

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".accordion-btn").forEach(button => {
-  button.addEventListener("click", () => {
-    const content = button.nextElementSibling;
-    content.style.display = content.style.display === "block" ? "none" : "block";
-  });
-});
